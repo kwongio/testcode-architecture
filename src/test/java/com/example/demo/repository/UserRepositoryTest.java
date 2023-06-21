80a06075d5a084ae43c341d2595819b85e161666
kwongio <rldh11111@naver.com>
2023-06-21 11:26:19 +0900

    [feat] : UserRepository 테스트 추가

---
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,73 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.UserStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DataJpaTest
+@Sql("/sql/user-repository-test-data.sql")
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @DisplayName("findByIdAndStatus로 유저를 조회할 수 있다.")
+    @Test
+    public void test5() throws IOException {
+        //given
+        //when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+
+        //then
+        assertThat(result.isPresent()).isTrue();
+
+    }
+
+    @DisplayName("findByIdAndStatus는 데이터가 없으면 Optional Emtpy를 반환한다.")
+    @Test
+    public void test6() throws IOException {
+        //given
+        //when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
+
+        //then
+        assertThat(result.isEmpty()).isTrue();
+
+    }
+
+    @DisplayName("findByIdAndStatus로 유저를 조회할 수 있다.")
+    @Test
+    public void test2() throws IOException {
+        //given
+        //when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("kwon@naver.com", UserStatus.ACTIVE);
+
+        //then
+        assertThat(result.isPresent()).isTrue();
+
+    }
+
+    @DisplayName("findByIdAndStatus는 데이터가 없으면 Optional Emtpy를 반환한다.")
+    @Test
+    public void test3() throws IOException {
+        //given
+
+        //when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("kwon@naver.com", UserStatus.PENDING);
+
+        //then
+        assertThat(result.isEmpty()).isTrue();
+
+    }
+
+}
