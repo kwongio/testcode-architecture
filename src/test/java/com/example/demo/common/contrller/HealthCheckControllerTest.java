ee94e66c4fffad7c862a4a4734691dc0a1e25ded
kwongio <rldh11111@naver.com>
2023-06-21 15:46:10 +0900

    [Style] : 테스트 패키지 구조 변경

--- src/test/java/com/example/demo/common/contrller/HealthCheckControllerTest.java
+++ src/test/java/com/example/demo/common/contrller/HealthCheckControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.common.contrller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
--- src/test/java/com/example/demo/post/controller/PostControllerTest.java
+++ src/test/java/com/example/demo/post/controller/PostControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.post.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
